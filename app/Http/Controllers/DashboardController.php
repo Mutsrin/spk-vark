e80deaa6381983d7dd209812bea252194039cdc2
Mutsrin <161734849+Mutsrin@users.noreply.github.com>
2026-04-14 18:58:32 +0700

    new env

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        dd(config('database.connections.mysql'));
         $totalResponden = KuesionerResponse::count();
         
         $gayaBelajarCount = [
